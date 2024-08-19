2d7fa90868cbb5564ad3a43b4ed0389cfd13ae64
jimin <jimin1286@gmail.com>
2024-08-19 10:29:08 +0900

    [Feat] delete entity

--- nest-blog/src/entities/base.entity.ts
+++ nest-blog/src/entities/base.entity.ts
@@ -2,12 +2,10 @@ import {
   BaseEntity as TypeOrmBaseEntity,
   CreateDateColumn,
   DeleteDateColumn,
-  Entity,
   UpdateDateColumn,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
-@Entity()
 export class BaseEntity extends TypeOrmBaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
